5c7decc97cac12284b357028b125299880883e73
seong-yeop <ucassje@ucl.ac.uk>
2021-04-30 00:05:23 +0100

    update

--- shift_code.py
+++ shift_code.py
@@ -79,12 +79,12 @@ for r in range(Nr):
         for j in range(Nv):
                 for i in range(Nv2):
                         if i%3==0:
-                                f_shift[r*(Nv)*(Nv)+j*Nv+i]=f_1[r*(Nv)*(Nv)+j*Nv+i//3]
+                                f_shift[r*(Nv)*(Nv2)+j*Nv2+i]=f_1[r*(Nv)*(Nv)+j*Nv+i//3]
                         elif (i-1)%3==0:
-                                f_shift[r*(Nv)*(Nv)+j*Nv+i]=f_1[r*(Nv)*(Nv)+j*Nv+(i-1)//3]+(1/3)*(f_1[r*(Nv)*(Nv)+j*Nv+(i+2)//3]-f_1[r*(Nv)*(Nv)+j*Nv+(i-1)//3])
+                                f_shift[r*(Nv)*(Nv2)+j*Nv2+i]=f_1[r*(Nv)*(Nv)+j*Nv+(i-1)//3]+(1/3)*(f_1[r*(Nv)*(Nv)+j*Nv+(i+2)//3]-f_1[r*(Nv)*(Nv)+j*Nv+(i-1)//3])
                         elif (i-2)%3==0:
                                 print(i)
-                                f_shift[r*(Nv)*(Nv)+j*Nv+i]=f_1[r*(Nv)*(Nv)+j*Nv+(i-2)//3]+(2/3)*(f_1[r*(Nv)*(Nv)+j*Nv+(i+1)//3]-f_1[r*(Nv)*(Nv)+j*Nv+(i-2)//3])
+                                f_shift[r*(Nv)*(Nv2)+j*Nv2+i]=f_1[r*(Nv)*(Nv)+j*Nv+(i-2)//3]+(2/3)*(f_1[r*(Nv)*(Nv)+j*Nv+(i+1)//3]-f_1[r*(Nv)*(Nv)+j*Nv+(i-2)//3])
 
 
 solu2=np.zeros(shape = (Nv2))
