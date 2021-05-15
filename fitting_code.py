c9ee178e9cceca62a03c961758e6c2a6b9121f2a
seong-yeop <ucassje@ucl.ac.uk>
2021-05-15 10:59:16 +0100

    update

--- fitting_code.py
+++ fitting_code.py
@@ -17,7 +17,7 @@ from numpy import exp, loadtxt, pi, sqrt
 from lmfit import Parameters, fit_report, minimize
 #from lmfit import Model
 import lmfit
-Nv=30  #velocity step number
+Nv=35  #velocity step number
 i_solar_r=5 #10
 f_solar_r=20 #30
 path_home="/Users/user/Desktop/JSY2/"
@@ -145,10 +145,10 @@ for r in range(Nr):
     print(r)
     if r==0:
             p = lmfit.Parameters()
-            p.add_many(('nc', 1,True,0.5,1),('ns', 0,True,0,0.5), ('Tc_pal', 10*10**5,True,1*10**5,10*10**5), ('Tc_per', 10*10**5,True,1*10**5,10*10**5), ('Ts_pal', 15*10**5,True,1*10**5,20*10**5), ('Ts_per', 15*10**5,True,1*10**5,20*10**5), ('Uc',0,True,-0.7,0),('Us',0.2,True,0,5),('kappac',10,True,4,50),('kappas',10,True,2,50))
+            p.add_many(('nc', 1,True,0.5,1),('ns', 0,True,0,0.5), ('Tc_pal', 10*10**5,True,1*10**5,10*10**5), ('Tc_per', 10*10**5,True,1*10**5,10*10**5), ('Ts_pal', 15*10**5,True,1*10**5,20*10**5), ('Ts_per', 15*10**5,True,1*10**5,20*10**5), ('Uc',0,True,-0.7,0),('Us',0.2,True,0,4),('kappac',10,True,4,50),('kappas',10,True,2,50))
     else:                          #,('Us',0,True,0,1.5) , ('Uc',0,True,-0.4,0) 
             p = lmfit.Parameters()
-            p.add_many(('nc', nc[r-1],True,0.7,1),('ns', ns[r-1],True,0,0.3), ('Tc_pal', Tc_pal[r-1],True,1*10**5,10*10**5), ('Tc_per', Tc_per[r-1],True,1*10**5,10*10**5), ('Ts_pal', Ts_pal[r-1],True,1*10**5,20*10**5), ('Ts_per', Ts_per[r-1],True,1*10**5,20*10**5), ('Uc',Uc[r-1],True,-0.7,0),('Us',Us[r-1],True,0,5),('kappac',kappac[r-1],True,4,50),('kappas',kappas[r-1],True,2,50))
+            p.add_many(('nc', nc[r-1],True,0.7,1),('ns', ns[r-1],True,0,0.3), ('Tc_pal', Tc_pal[r-1],True,1*10**5,10*10**5), ('Tc_per', Tc_per[r-1],True,1*10**5,10*10**5), ('Ts_pal', Ts_pal[r-1],True,1*10**5,20*10**5), ('Ts_per', Ts_per[r-1],True,1*10**5,20*10**5), ('Uc',Uc[r-1],True,-0.7,0),('Us',Us[r-1],True,0,4),('kappac',kappac[r-1],True,4,50),('kappas',kappas[r-1],True,2,50))
                                    #,('Us',Us,True,0,1.5) , ('Uc',Uc,True,-0.4,0.4) 
     f_11=np.zeros(shape = (Nv**2, 1))
     for j in range(Nv):
