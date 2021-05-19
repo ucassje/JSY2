e16b84197475e71195e11883790711cb910df219
seong-yeop <ucassje@ucl.ac.uk>
2021-05-19 14:36:33 +0100

    update

--- fitting_code.py
+++ fitting_code.py
@@ -17,7 +17,7 @@ from numpy import exp, loadtxt, pi, sqrt
 from lmfit import Parameters, fit_report, minimize
 #from lmfit import Model
 import lmfit
-Nv=36  #velocity step number
+Nv=34  #velocity step number
 i_solar_r=5 #10
 f_solar_r=20 #30
 path_home="/Users/user/Desktop/JSY2/"
@@ -36,7 +36,7 @@ q=1.6022*(10**(-19))
 Me=9.1094*(10**(-31))
 Mp=1.6726*(10**(-27))
 ratio=(Me/Mp)**0.5
-Mv=12*10**6/v_Ae_0  #5*10**7 #(2/3)*5*10**7 
+Mv=15*10**6/v_Ae_0  #5*10**7 #(2/3)*5*10**7 
 epsilon=8.8542*10**(-12)
 pal_v = np.linspace(-Mv, Mv, Nv)
 per_v = np.linspace(-Mv, Mv, Nv)
