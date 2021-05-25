26ee33f7a84c64b1c6c37eeea82fb84cfa6c7f31
seong-yeop <ucassje@ucl.ac.uk>
2021-05-25 22:09:25 +0100

    update

--- EulerCN.py
+++ EulerCN.py
@@ -670,17 +670,17 @@ for k in range(timestep):
                                     if j==Nv-1 and i!=0 and i!=Nv-1:
                                             f_temp1[j*Nv+i,r]=f_1[(j-1)*Nv+i+1,r]*(f_1[(j-1)*Nv+i-1,r]/f_1[(j-2)*Nv+i,r])#2*f_1[(r)*(Nv)*(Nv)+(j-1)*Nv+i]-f_1[(r)*(Nv)*(Nv)+(j-2)*Nv+i] #np.max(f_1)*10**(2*np.log10(f_1[(r)*(Nv)*(Nv)+(j-1)*Nv+i]/np.max(f_1))-np.log10(f_1[(r)*(Nv)*(Nv)+(j-2)*Nv+i]/np.max(f_1)))                                #((per_v[j]-per_v[j-2])/(per_v[j-2]-per_v[j-1]))*(f_1[(r)*(Nv)*(Nv)+(j-2)*Nv+i]-f_1[(r)*(Nv)*(Nv)+(j-1)*Nv+i])+f_1[(r)*(Nv)*(Nv)+(j-2)*Nv+i]                                                                                
                                                 
-                                    if j==0 and i==0:
-                                            f_temp1[j*Nv+i,r]=f_1[(j)*Nv+i+1,r]*(f_1[(j)*Nv+i+1,r]/f_1[(j)*Nv+i+2,r])
+                                    #if j==0 and i==0:
+                                    #        f_temp1[j*Nv+i,r]=f_1[(j)*Nv+i+1,r]*(f_1[(j)*Nv+i+1,r]/f_1[(j)*Nv+i+2,r])
                                                 
-                                    if j==Nv-1 and i==Nv-1:
-                                            f_temp1[j*Nv+i,r]=f_1[(j)*Nv+i-1,r]*(f_1[(j)*Nv+i-1,r]/f_1[(j)*Nv+i-2,r])
+                                    #if j==Nv-1 and i==Nv-1:
+                                    #        f_temp1[j*Nv+i,r]=f_1[(j)*Nv+i-1,r]*(f_1[(j)*Nv+i-1,r]/f_1[(j)*Nv+i-2,r])
 
-                                    if j==0 and i==Nv-1:
-                                            f_temp1[j*Nv+i,r]=f_1[(j)*Nv+i-1,r]*(f_1[(j)*Nv+i-1,r]/f_1[(j)*Nv+i-2,r])
+                                    #if j==0 and i==Nv-1:
+                                    #        f_temp1[j*Nv+i,r]=f_1[(j)*Nv+i-1,r]*(f_1[(j)*Nv+i-1,r]/f_1[(j)*Nv+i-2,r])
                                                 
-                                    if j==Nv-1 and i==0:
-                                            f_temp1[j*Nv+i,r]=f_1[(j)*Nv+i+1,r]*(f_1[(j)*Nv+i+1,r]/f_1[(j)*Nv+i+2,r])
+                                    #if j==Nv-1 and i==0:
+                                    #        f_temp1[j*Nv+i,r]=f_1[(j)*Nv+i+1,r]*(f_1[(j)*Nv+i+1,r]/f_1[(j)*Nv+i+2,r])
         f_1[:,:]=f_temp1[:,:]
         f_1[:,0]=f_initial[:,0]
 
